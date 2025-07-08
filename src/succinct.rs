957430a459f7a2332ab5bab4a12f9b473bb95c87
Tej Qu Nair <tej.q.nair@gmail.com>
2025-07-08 05:35:18 +0000

    tiny-keccak 2.0.2 v6 patch

--- src/succinct.rs
+++ src/succinct.rs
@@ -1,7 +1,3 @@
-extern "C" {
-    fn syscall_keccak_permute(w: *mut u64);
-}
-
 /// Executes the Keccak256 permutation on the given state.
 ///
 /// ### Safety
@@ -13,6 +9,6 @@ extern "C" {
 #[inline]
 pub fn keccakf(state: &mut [u64; 25]) {
     unsafe {
-        syscall_keccak_permute(state.as_mut_ptr());
+        sp1_lib::syscall_keccak_permute(state);
     }
 }
